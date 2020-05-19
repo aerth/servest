4d7e2422a2f7b5989cc01e8e9cc6a73c3d8f3005
aerth <aerth@riseup.net>
2020-05-19 22:50:20 +0000

    servest v0.2.0

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 aerth
+// Copyright (c) 2016-2020 aerth
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,58 +20,122 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// Servest is a quick http server with a few options
+// Servest is a quick http fileserver with a few options
 package main
 
 import (
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
-var port int
-var in string
-var dir string
-var servepath string
-var portMin = 8000
-var portMax = 8999
+var (
+	port      int
+	in        string
+	dir       string
+	servepath string
+	portMin   = 8000
+	portMax   = 8999
+	logging   = false // to stderr
+)
+
+const (
+	repolink = "https://github.com/aerth/servest"
+)
+
+var (
+	Version     = "0.2.x-unknown"
+	DefaultDir  string // cwd if empty
+	DefaultBind = "127.0.0.1"
+)
 
 func init() {
 	flag.IntVar(&port, "p", 0, "Port to listen on (default: 0, look for free port)")
-	flag.StringVar(&in, "i", "0.0.0.0", "Interface to listen on (default: 0.0.0.0)")
-	flag.StringVar(&dir, "d", "", "Directory to serve (default: cwd)")
+	flag.StringVar(&in, "i", DefaultBind, "Interface to listen on")
+	flag.StringVar(&dir, "d", DefaultDir, "Directory to serve (current working dir if empty)")
 	flag.IntVar(&portMin, "minport", 8000, "Minimum port to try binding to")
 	flag.IntVar(&portMax, "maxport", 8999, "Maximum port to try binding to")
+	flag.BoolVar(&logging, "log", false, "Enable http request logging")
 }
 
 func main() {
+	var showVersion = false
+	flag.BoolVar(&showVersion, "version", false, "Show version information and exit")
 	flag.Parse()
-	fmt.Println("[servest]")
-	fmt.Println("https://github.com/aerth/servest")
+	if flag.NArg() != 0 {
+		fmt.Fprintf(os.Stderr, "[servest] server require no arguments, only command line flags. see -h to print flags and defaults\n")
+		os.Exit(111)
+	}
+	if showVersion {
+		fmt.Printf("servest v%s\n", Version)
+		os.Exit(0)
+	}
+	fmt.Fprintln(os.Stderr, "[servest] starting...")
+	fmt.Fprintln(os.Stderr, "[servest] source code: https://github.com/aerth/servest")
+	t1 := time.Now()
 
 	// User defined a directory to serve
 	if dir != "" {
 		servepath = dir
 	} else {
-		// Else we serve current working directory
-		servepath, _ = os.Getwd()
+		// Else we serve current working directory (if possible)
+		var err error
+		servepath, err = os.Getwd()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[servest] fatal: %v", err)
+			os.Exit(222)
+		}
+		fmt.Fprintln(os.Stderr, "[servest] serving current working dir:", servepath)
 	}
 
 	// User defined a port for binding
 	if port != 0 {
-		fmt.Printf("\nServing %s on %s:%d\n", servepath, in, port)
-		fmt.Println(http.ListenAndServe(fmt.Sprintf("%s:%d", in, port), http.FileServer(http.Dir(servepath))))
+		fmt.Fprintf(os.Stderr, "[servest] serving %s on %s:%d\n", servepath, in, port)
+		fmt.Fprintln(os.Stderr, http.ListenAndServe(fmt.Sprintf("%s:%d", in, port), http.FileServer(http.Dir(servepath))))
 		os.Exit(1)
 	}
 
-	fmt.Printf("\nServing %s on %s\n", servepath, in)
-	fmt.Printf("\nLooking for an available port between %d and %d \n", portMin, portMax)
+	fmt.Fprintf(os.Stderr, "[servest] serving %s on %s (first available port)\n", servepath, in)
+	fmt.Fprintf(os.Stderr, "[servest] looking for an available port between %d and %d \n", portMin, portMax)
 	// Here we search for an open port within the boundries of portMin and portMax.
 	for port := portMin; port <= portMax; port++ {
 		// We print the port we are *trying* to bind to, if it isn't possible we keep trying different ports.
-		fmt.Printf("Port: %d.\n", port)
-		fmt.Println(http.ListenAndServe(fmt.Sprintf("%s:%d", in, port), http.FileServer(http.Dir(servepath))))
+		go func() {
+			<-time.After(time.Second)
+			fmt.Fprintf(os.Stderr, "[servest] listening on port: %d.\n", port)
+		}()
+		err := http.ListenAndServe(fmt.Sprintf("%s:%d", in, port), wrapHandler{http.FileServer(http.Dir(servepath))})
+		if !strings.Contains(err.Error(), "already in use") {
+			fmt.Fprintln(os.Stderr, "[servest] error:", err)
+			if time.Since(t1) < time.Second*3 {
+				fmt.Fprintln(os.Stderr, "[servest] server encountered boot error. please report this as an issue at the source code repository:", repolink)
+			}
+			os.Exit(111)
+		}
 	}
 	os.Exit(1)
+}
+
+type wrapHandler struct {
+	underlyingHandler http.Handler
+}
+
+func trimPort(s string) string {
+	host, _, err := net.SplitHostPort(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[servest] error: %s\n", err.Error())
+	}
+	return host
+}
+
+func (www wrapHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// insert any routers or www middleware handlers you need here?
+	if logging {
+		fmt.Fprintf(os.Stderr, "[servest] %s %s %s %s %s - %s %s\n", time.Now().Format(time.ANSIC), r.Method, r.Host, r.URL.Path, r.URL.RawPath, trimPort(r.RemoteAddr), r.UserAgent())
+	}
+	www.underlyingHandler.ServeHTTP(w, r)
 }
